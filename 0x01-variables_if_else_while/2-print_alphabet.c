71dd70ddc6dbafed2dca4682ff290b2bd4683173
kokol12 <kokoleismail886@gmail.com>
2023-04-10 22:10:25 +0300

    printing alpbets using ascii numbers

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: returns 0
+*/
+
+int main(void)
+{
+    char a;
+
+    for(a = 97; a < 123; a++)
+    {
+        putchar(a);
+    }
+    putchar('\n');
+    return (0);
+}
