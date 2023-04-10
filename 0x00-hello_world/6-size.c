c212a133603063a57d6078fb2514622b05dd88a4
kokol12 <kokoleismail886@gmail.com>
2023-04-10 21:27:27 +0300

    printing sizes of diffewrent datatypes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: returns 0
+*/
+
+int main(void)
+{
+    printf("Size of char: %lu byte(s)\n", sizeof(char));
+    printf("Size of an int: %lu byte(s)\n", sizeof(int));
+    printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+}
