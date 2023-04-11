7cbe085129c0551f5636deb305792ea60392beb7
kokol12 <kokoleismail886@gmail.com>
2023-04-11 20:23:35 +0300

    puts function to print to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * 
+*/
+
+void _puts(char *str)
+{
+    int i;
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+    _putchar('\n');
+}
