1754fd4ce26984221ba14589510f44aeb4126fe1
kokol12 <kokoleismail886@gmail.com>
2023-04-13 12:21:09 +0300

    reverse array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * 
+ * 
+*/
+
+void reverse_array(int *a, int n)
+{
+    int i, h;
+
+    for (i = 0; i < (n / 2); i++)
+    {
+        h = a[i];
+        a[i] = a[n - i - 1];
+        a[n - i - 1] = h;
+    }
+}
