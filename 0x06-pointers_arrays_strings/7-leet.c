191ddb1b1ed36e1edd11c89df4b7c10ac1216879
kokol12 <kokoleismail886@gmail.com>
2023-04-14 22:19:41 +0300

    leet character formatting

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * 
+ * 
+ * 
+*/
+
+char *leet(char *s)
+{
+    int i, j;
+    char *a = "aAeEoOtTIL";
+    char *b = "4433007711";
+
+    for (i = 0; s[i] != '0'; i++)
+    {
+        for (j = 0; j < 10; j++)
+        {
+            if (s[i] == a[j])
+            {
+                s[i] = b[j];
+            }
+        }
+    }
+    return (s);
+}
